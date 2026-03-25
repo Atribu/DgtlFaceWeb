fe3f6c17d66497aefb44d26a35c0e51a7cb5179d
Cenk Arslan <c.arslan3509@gmail.com>
2026-03-25 10:23:13 +0300

    updt1

--- client/app/[locale]/blogs/layout.js
+++ client/app/[locale]/blogs/layout.js
@@ -1,15 +1,3 @@
-import { NextIntlClientProvider } from "next-intl";
-import { getMessages, setRequestLocale } from "next-intl/server";
-
-export default async function BlogsPageLayout({ children, params }) {
-  const { locale } = await params;
-  setRequestLocale(locale);
-
-  const messages = await getMessages();
-
-  return (
-    <NextIntlClientProvider locale={locale} messages={messages}>
-      {children}
-    </NextIntlClientProvider>
-  );
+export default function BlogsPageLayout({ children }) {
+  return children;
 }
