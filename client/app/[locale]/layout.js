f66dc3953b42a4abaa9020a2118e936548b8e6eb
ismetkurgan <ismetkurgan@outlook.com>
2026-04-17 19:16:41 +0000

    Use local Inter fonts instead of Google Fonts to fix build retrying issue

--- client/app/[locale]/layout.js
+++ client/app/[locale]/layout.js
@@ -7,7 +7,7 @@ import HeaderWrapper from "./components/HeaderWrapper";
 import { getSeoData } from '../lib/seo-utils'; 
 import { getSiteUrl } from "@/app/lib/site-url";
 import Script from "next/script";
-import { Inter } from "next/font/google";
+import localFont from "next/font/local";
 import dynamic from 'next/dynamic';
 import {
   CookiePopupDeferred,
@@ -17,11 +17,15 @@ import {
 
 const Footer = dynamic(() => import("./components/footer/Footer"));
 
-const inter = Inter({
-  subsets: ["latin"],
+const inter = localFont({
+  src: [
+    { path: "../../public/fonts/inter-400.woff2", weight: "400", style: "normal" },
+    { path: "../../public/fonts/inter-500.woff2", weight: "500", style: "normal" },
+    { path: "../../public/fonts/inter-600.woff2", weight: "600", style: "normal" },
+    { path: "../../public/fonts/inter-700.woff2", weight: "700", style: "normal" },
+  ],
   display: "swap",
-  weight: ["400", "500", "600", "700"],
-  variable: "--font-inter", 
+  variable: "--font-inter",
 });
 
 function buildClientMessages(allMessages) {
